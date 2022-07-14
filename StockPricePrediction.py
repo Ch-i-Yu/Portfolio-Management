e98b249094c8769d11a3cfb32daa2f096b502e2d
Ch-i-Yu <scyqy1@nottingham.edu.cn>
2022-07-14 21:12:51 +0800

    Set Dependencies

--- StockPricePrediction.py
+++ StockPricePrediction.py
@@ -1,3 +1,30 @@
+# import necessary dependencies
+
+from warnings import simplefilter
+simplefilter(action = "ignore", category = FutureWarning)
+simplefilter(action = "ignore", category = DeprecationWarning)
+
+import gc
+import os
+import re
+
+import numpy as np
+import pandas as pd
+np.random.seed(42)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.color_palette("rocket_r", as_cmap=True)
+sns.set_style("white")
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout, Bidirectional, GRU
+
+
 class StockPricePrediction:
     """Predict Future Stock Prices using Recurrent Neural Network with Long Short-Term Memory."""
     def _init_(self):
