0589b42e39fda4ba1356dbf7874c394b73aebe02
Ch-i-Yu <scyqy1@nottingham.edu.cn>
2022-07-14 21:00:53 +0800

    Add StockPricePrediction.py: Initialize Class

---
+++ StockPricePrediction.py
@@ -0,0 +1,7 @@
+class StockPricePrediction:
+    """Predict Future Stock Prices using Recurrent Neural Network with Long Short-Term Memory."""
+    def _init_(self):
+        self.args = None
+
+    def _init_(self, args):
+        self.args = args
