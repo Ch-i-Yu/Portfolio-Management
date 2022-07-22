a923f1b492679ee4261daeea2a43fc96a99937b7
Ashley <77085901+Ashley-Willkes@users.noreply.github.com>
2022-07-22 15:46:33 +0800

    Update PastPaperTrading.py

--- PastPaperTrading.py
+++ PastPaperTrading.py
@@ -12,8 +12,10 @@ def pastPaperTrading(period, start, equities, portfolios):
     profits = []
 
     for equity in equities:
-        file_relative = equity+".csv"
+        file_relative = "./Stock-Dataset/" + equity +".csv"
         result_csv = load_csv(file_relative)
+        # result_csv = pd.read_csv(file_relative)
+        result_csv = result_csv.drop(["Open", "High", "Low" , "Close" , "Volume"], axis=1)
         datas[equity] = result_csv
 
     time_offset = 0
@@ -26,37 +28,44 @@ def pastPaperTrading(period, start, equities, portfolios):
                 df = datas[equity]
                 newdata = pd.DataFrame(columns=None);
                 count = 0
-                for i in df[0]:
+                record = 0
+                for i in df["Date"]:
                     if i == start:
                         break
+                    elif i == "2019-01-02":
+                        record = count
+                        count += 1
                     else:
                         count += 1
                 destination.append(count)
-                insert = df[0:count + time_offset + 2]
+                # +2是因为要计算利润 引入第二天的数据
+                insert = df[record:count + time_offset + 2]
                 df_new = newdata.append(insert)
 
-                # 删除预测列
-                df_new = df_new.drop([2], axis=1)
-                df_new.rename(columns={1: equity}, inplace=True)
+                # 以股票名字重命名
+                df_new.rename(columns={"Adj Close": equity}, inplace=True)
                 is_first = False
 
             else:
                 df_copy = datas[equity]
                 newdata_copy = pd.DataFrame(columns=None);
                 count = 0
-                for i in df_copy[0]:
+                record = 0
+                for i in df_copy["Date"]:
                     if i == start:
                         break
+                    elif i == "2019-01-02":
+                        record = count
+                        count += 1
                     else:
                         count += 1
                 destination.append(count)
-                insert = df_copy[0:count + time_offset + 2]
+                insert = df_copy[record:count + time_offset + 2]
                 df_new_copy = newdata_copy.append(insert)
 
                 # 删除预测列
-                df_new_copy = df_new_copy.drop([2], axis=1)
-                df_new_copy = df_new_copy.drop([0], axis=1)
-                df_new_copy.rename(columns={1: equity}, inplace=True)
+                df_new_copy = df_new_copy.drop(["Date"], axis=1)
+                df_new_copy.rename(columns={"Adj Close": equity}, inplace=True)
 
                 # 将新的股票列添加进来
                 columns = df_new.columns.tolist()
